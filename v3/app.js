690e990d080603a3fbdd4ee24c0c039419c1551e
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-02-24 11:56:19 +0000

    Added v3 version of YelpCamp; Refactored campgound model in campground.js

---
+++ v3/app.js
@@ -0,0 +1,75 @@
+var express     = require("express"),
+    app         = express(),
+    bodyParser  = require("body-parser"),
+    mongoose    = require("mongoose"),
+    Campground  = require("./models/campground");
+    // Comment     = require("./models/comment"),
+    // User        = require("./models/user")
+
+mongoose.connect("mongodb://localhost:27017/yelp_camp", {useNewUrlParser: true});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+// Campground.create({
+//         name: "Granite Hill",
+//         image: "https://cdn.pixabay.com/photo/2015/03/26/10/29/camping-691424_960_720.jpg",
+//         description: "Beutiful camp"
+//     }, function(err, newlyCreated) {
+//         if(err){
+//             console.log(err);
+//         }
+//     });
+
+app.get("/", function(req, res){
+    res.render("landing");
+});
+
+
+app.get("/campgrounds", function(req, res){
+
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {campgrounds: allCampgrounds});
+        }
+    });
+})
+
+
+app.post("/campgrounds", function(req, res){
+     var newCampground = {
+         name: req.body.name,
+         image: req.body.image,
+         description: req.body.description
+     }
+     Campground.create(newCampground, function(err, newlyCreated){
+         if(err){
+             console.log(err);
+         }else{
+            res.redirect("/campgrounds"); 
+         }
+     });
+});
+
+
+app.get("/campgrounds/new", function(req, res) {
+   res.render("new.ejs") ;
+});
+
+
+app.get("/campgrounds/:id", function(req, res) {
+    var id = req.params.id;
+    Campground.findById(id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("show", {campground: foundCampground});
+        }
+    });
+})
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Server started...");
+});

6ead422f3abf610ab7eea779c5dcc735765f0259
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-03-01 12:35:59 +0000

    Added functionality to edit comment

--- v9/routes/campgrounds.js
+++ v9/routes/campgrounds.js
@@ -59,20 +59,14 @@ router.get("/:id", function(req, res) {
 });
 
 // EDIT
-router.get("/:id/edit", function(req, res) {
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
     Campground.findById(req.params.id, function(err, foundCampground){
-        if(err){
-            console.log(err);
-        } else{
-            res.render("campgrounds/edit", {
-                campground: foundCampground
-            });  
-        }
+        res.render("campgrounds/edit", {campground: foundCampground});
     });
 });
 
 // UPDATE
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
        if(err){
            res.redirect("/campgrounds");
@@ -83,8 +77,7 @@ router.put("/:id", function(req, res){
 });
 
 // DESTROY
-
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
     Campground.findByIdAndRemove(req.params.id, function(err){
         if(err){
             res.redirect("/campgrounds");
@@ -100,6 +93,24 @@ function isLoggedIn(req, res, next){
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCampgroundOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Campground.findById(req.params.id, function(err, foundCampground){
+            if(err){
+                res.redirect("back");
+            } else{
+                if(foundCampground.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 module.exports = router;

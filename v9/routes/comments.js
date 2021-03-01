9918cd03a476f5663fb72e1a6392967854a7a76b
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-03-01 12:51:35 +0000

    Added delele comment functionality

--- v9/routes/comments.js
+++ v9/routes/comments.js
@@ -67,6 +67,17 @@ router.put("/:comment_id", function(req, res){
     });
 })
 
+//DELETE
+router.delete("/:comment_id", function(req, res){
+    Comment.findByIdAndRemove(req.params.comment_id, function(err){
+        if(err){
+            res.redirect("back");
+        } else{
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    });
+});
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();

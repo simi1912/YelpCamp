4eaf853fc6c22ef4403ec82900cfc082a1d13f83
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-03-01 16:43:37 +0000

    Added functionlaity that hides edit/delete button from non-authors

--- v9/routes/comments.js
+++ v9/routes/comments.js
@@ -41,7 +41,7 @@ router.post("/", isLoggedIn, function(req, res){
 })
 
 // EDIT
-router.get("/:comment_id/edit", function(req, res){
+router.get("/:comment_id/edit", checkCommentOwnership, function(req, res){
     Comment.findById(req.params.comment_id, function(err, foundComment) {
         if(err){
             res.redirect("back");
@@ -57,7 +57,7 @@ router.get("/:comment_id/edit", function(req, res){
 });
 
 // UPDATE
-router.put("/:comment_id", function(req, res){
+router.put("/:comment_id", checkCommentOwnership, function(req, res){
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, function(err, updatedComment){
         if(err){
             res.redirect("back");
@@ -68,7 +68,7 @@ router.put("/:comment_id", function(req, res){
 })
 
 //DELETE
-router.delete("/:comment_id", function(req, res){
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
     Comment.findByIdAndRemove(req.params.comment_id, function(err){
         if(err){
             res.redirect("back");
@@ -77,6 +77,24 @@ router.delete("/:comment_id", function(req, res){
         }
     });
 });
+
+function checkCommentOwnership(req, res, next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id, function(err, foundComment){
+            if(err){
+                res.redirect("back");
+            } else{
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
+}
 
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){

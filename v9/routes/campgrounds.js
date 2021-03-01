6f7d33efe70229da3e5d0009f96c59d5f354b30e
EC2 Default User <ec2-user@ip-172-31-25-217.us-east-2.compute.internal>
2021-03-01 09:07:44 +0000

    Added editing campground functionality

--- v9/routes/campgrounds.js
+++ v9/routes/campgrounds.js
@@ -58,6 +58,30 @@ router.get("/:id", function(req, res) {
     });
 });
 
+// EDIT
+router.get("/:id/edit", function(req, res) {
+    Campground.findById(req.params.id, function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/edit", {
+                campground: foundCampground
+            });  
+        }
+    });
+});
+
+//Update
+router.put("/:id", function(req, res){
+    Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
+       if(err){
+           res.redirect("/campgrounds");
+       } else {
+           res.redirect("/campgrounds/" + req.params.id);
+       }
+    });
+});
+
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated()){
         return next();
